436203c3e7e4ec1df3bfec6413c0768ddbbf5d3d
lz88398022 <1399653996@qq.com>
2024-09-21 17:02:39 +0800

    测试上传前端代码
    测试上传前端代码

---
+++ selfBlogFront/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import useSettingsStore from '@/store/modules/settings'
+import { handleThemeStyle } from '@/utils/theme'
+
+onMounted(() => {
+  nextTick(() => {
+    // 初始化主题样式
+    handleThemeStyle(useSettingsStore().theme)
+  })
+})
+</script>
+
+<style lang="scss">
+@import './App.scss';
+</style>
