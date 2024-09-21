0444dda3a1921247ed25b22ee688913bc660f13d
lz88398022 <1399653996@qq.com>
2024-09-21 16:52:42 +0800

    初始化后端代码

---
+++ selfBlogBackend/src/test/java/com/blog/hulk/selfblog/SelfBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.hulk.selfblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
